5b77f45774e8a2edc653856f669426529658c1e6
Asia Kane <asialkane@gmail.com>
2017-04-16 11:04:37 -0700

    add form.erb and greeting_card.erb and updated app.rb to take user input from form and display on greeting_card page

--- app.rb
+++ app.rb
@@ -2,9 +2,20 @@ require('sinatra')
 require('sinatra/reloader')
 
 get('/') do
+  @friend = "Asia"
   erb(:letter)
 end
 
 get('/favorite_photos') do
   erb(:favorite_photos)
+end
+
+get('/form') do
+  erb(:form)
+end
+
+get('/greeting_card') do
+  @person1 = params.fetch('person1')
+  @person2 = params.fetch('person2')
+  erb(:greeting_card)
 end
